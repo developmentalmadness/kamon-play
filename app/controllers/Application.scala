2eb53648517e320eb1c93a930f47eba00d7597b4
unknown <mark.miller@SDYRNDMMILLER.ucn.net>
2015-12-07 17:48:39 -0700

    adding akka actors to test kamon-akka

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,26 @@
 package controllers
 
+import javax.inject.Inject
+
+import actors.MessageGenerator.{ConstantLoad, Peak, Schedule}
+import actors.{RandomNumberActor, MessageGeneratorActor}
+import actors.RandomNumberActor.{GenerateSecureNumber, GenerateNumber}
+import akka.actor.{Props, ActorSystem}
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject() (system: ActorSystem) extends Controller {
 
   def index = Action {
+    val numberGenerator = system.actorOf(Props[RandomNumberActor])
+
+    val generator = system.actorOf(Props[MessageGeneratorActor])
+
+    generator ! ConstantLoad(Schedule(numberGenerator, GenerateNumber, 5000))
+    generator ! ConstantLoad(Schedule(numberGenerator, GenerateSecureNumber, 1000))
+    generator ! Peak(Schedule(numberGenerator, GenerateNumber, 100000))
+    generator ! Peak(Schedule(numberGenerator, GenerateSecureNumber, 25000))
+
     Ok(views.html.index("Your new application is ready."))
   }
 
